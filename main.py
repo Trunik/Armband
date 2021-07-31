ee785c92b53a04d47f03b9be06341e7dd43a5266
Trunik <pagan2617@protonmail.com>
2021-07-30 21:03:47 -0400

    Now posting main.py for real ;)

---
+++ main.py
@@ -0,0 +1,11 @@
+# using Micropython for this project mostly due to the ability 
+# to use both processors in the RP2040. Circuitpython can not
+# do that as of this writing, but Version 7 of CircuitPython 
+# will be released soon.
+
+import from machine UART, SPI, I2C, Pin
+import DS3231
+import Adafruit_ESP_control
+import BME280
+import LSM303A
+
